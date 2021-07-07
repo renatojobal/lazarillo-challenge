680f87e9de3cd05adaef2965223ce830584986e4
renatojobal <renatojobal@gmail.com>
2021-07-07 16:16:52 -0500

    :bug: use the place ass an interface

--- src/app/models/place.model.ts
+++ src/app/models/place.model.ts
@@ -1,10 +1,10 @@
-export class Place {
-    id: string = '';
-    name: string = '';
-    latitude: string = '';
-    longitude: string = '';
-    topLeftCornerBox: string = ''; 
-    topRightConerBox: string = '';
-    bottomLeftCornerBox: string = '';
-    bottomRightCornerBox: string = ''; 
+export interface Place {
+    id: string,
+    name: string,
+    latitude: string,
+    longitude: string,
+    topLeftCornerBox: string,
+    topRightConerBox: string,
+    bottomLeftCornerBox: string,
+    bottomRightCornerBox: string,
 }
