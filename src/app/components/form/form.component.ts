e0a79fd32a1875061567b3fd57df2331d1c7bea8
renatojobal <renatojobal@gmail.com>
2021-07-07 16:11:50 -0500

    :art: allow the user to add a new place

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PlaceService } from 'src/app/services/place.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  public placeForm: FormGroup
+
+  constructor(
+    public placeService: PlaceService,
+    public formBuilder: FormBuilder,
+    public router: Router
+  ) { 
+    this.placeForm = this.formBuilder.group({
+      name: ['']
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit (){
+    console.log("Store this place information")
+    this.placeService.createPlace(this.placeForm.value);
+    
+  }
+
+
 
 }
