11deb25578f14272dffb9b3f26a7eb25719999ae
renatojobal <renatojobal@gmail.com>
2021-07-07 16:10:11 -0500

    :sparkles: service for get the docs from firestore

---
+++ src/app/services/place.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Place } from '../models/place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaceService {
+
+  constructor(private angularFirestore: AngularFirestore) {}
+
+  getPlaceDoc(id: string){
+    return this.angularFirestore
+    .collection('place-collection')
+    .doc(id)
+    .valueChanges()
+  }
+
+  getPlaceList(){
+    return this.angularFirestore
+    .collection("place-collection")
+    .snapshotChanges();
+  }
+
+  createPlace(place: Place){
+    return new Promise<any>((resolve, reject) => {
+      this.angularFirestore
+      .collection("place-collection")
+      .add(place)
+      .then(response => { console.log(response) }, error => reject(error))
+    });
+  }
+
+  deletePlace(place: Place, id: string){
+    return this.angularFirestore
+    .collection("place-collection")
+    .doc(id)
+    .update({
+      name: place.name,
+      latitude: place.latitude,
+      longitude: place.longitude,
+      topLeftCornerBox: place.topLeftCornerBox,
+      topRightConerBox: place.topRightConerBox,
+      bottomLeftCornerBox: place.bottomLeftCornerBox,
+      bottomRightCornerBox: place.bottomRightCornerBox 
+    })
+  }
+
+
+}
