0f00bc19e7aa6c32ceab8d5620276b25bc46d9c2
renatojobal <renatojobal@gmail.com>
2021-07-10 17:52:31 -0500

    :art: add form to let user searh places
    The user could search a place by latitude and longitude or selecting a popular place

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,36 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { PlaceService } from 'src/app/services/place.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Place } from 'src/app/models/place.model';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  public geoPointForm: FormGroup;
 
-  public placeForm: FormGroup
+  // List of popular places
+  popularPlace$: Observable<any[]>;
+
+  selectedPopularPlace = { name: 'Ninguno seleccionado' };
 
   constructor(
     public placeService: PlaceService,
     public formBuilder: FormBuilder,
     public router: Router
-  ) { 
-    this.placeForm = this.formBuilder.group({
-      name: ['']
-    })
+  ) {
+    this.geoPointForm = this.formBuilder.group({
+      "latitude": [""],
+      "longitude": [""]
+    });
+
+    // Update the list of places from firebase
+    this.popularPlace$ = placeService.getPopularPlaceList();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // Event emitter
+  @Output() changedPlaceEvent = new EventEmitter<Place>();
+
+  onSubmit() {
+    console.log('Searching by lat and long'+this.geoPointForm.value);
+
+    this.changedPlaceEvent.emit(this.geoPointForm.value); // Emit the selected place to app component
   }
 
-  onSubmit (){
-    console.log("Store this place information")
-    this.placeService.createPlace(this.placeForm.value);
-    
+  onPopularPlaceSelected(place: Place) {
+    console.log('Searching a popular place: ' + place.name);
+    this.changedPlaceEvent.emit(place); // Emit the selected place to app component
   }
-
-
-
 }
